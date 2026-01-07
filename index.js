8293b61b5b0688b89d2555d7324a82a28ca55245
HorrySheet11 <marinnathan11@gmail.com>
2026-01-07 13:25:38 +0800

    successfully used node express for routing
    added all files
    used index.js for routing

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('node:path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+app.get('/about', (req,res) => {
+  res.sendFile(`${__dirname}/public/about.html`);
+});
+
+app.get('/contact-me', (req,res) => {
+  res.sendFile(`${__dirname}/public/contact-me.html`);
+});
+
+app.use((req,res) => {
+  res.status(404);
+  res.sendFile(`${__dirname}/public/404.html`);
+});
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
